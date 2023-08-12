ee17d8d5d50bac5d3a7ce0f34b70dedf3407563e
Gulácsi András <guland@protonmail.com>
2023-08-12 02:59:12 +0200

    redirect to login page if session expires after unsuccessful axios call

--- resources/spa/api/Api.js
+++ resources/spa/api/Api.js
@@ -10,4 +10,27 @@ axios.interceptors.request.use(function (config) {
     return config;
 });
 
+axios.interceptors.response.use(
+    function(response) {
+        // Call was successful, don't do anything special.
+        return response;
+    },
+    function (error) {
+        switch (error.response.status) {
+            case 401: // Not logged in
+            case 419: // Session expired
+            case 503: // Down for maintenance
+                // Bounce the user to the login screen with a redirect back
+                window.location.reload();
+                break;
+            case 500:
+                console.error('Oops, something went wrong! The team have been notified.');
+                break;
+            default:
+                // Allow individual requests to handle other errors
+                return Promise.reject(error);
+        }
+    });
+
+
 export default axios;
